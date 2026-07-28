ab18bf349ccbf62dba55614fc5aeb85d5357b9ab
Mogvl <979758106@qq.com>
2026-07-28 13:38:07 +0800

    Fix weekly: prevent premature load, show data on init

--- web/src/views/Weekly.vue
+++ web/src/views/Weekly.vue
@@ -2,14 +2,14 @@
   <div class="week-page">
     <!-- 顶部：周选择 + 更新提示 -->
     <div class="week-header">
-      <el-select v-model="selectedWeek" @change="loadData" placeholder="选择周期" class="week-select">
+      <el-select v-model="selectedWeek" @change="onWeekChange" placeholder="选择周期" class="week-select" :loading="initLoading">
         <el-option v-for="(cat, i) in weekCategories" :key="i" :label="cat.time + ' - ' + cat.title" :value="cat.id" />
       </el-select>
       <span class="update-tip">每周五 18:00更新</span>
     </div>
 
     <!-- 日漫/韩漫/其他 标签页 -->
-    <el-tabs v-model="activeType" @tab-change="loadData" class="week-tabs">
+    <el-tabs v-model="activeType" @tab-change="onTabChange" class="week-tabs">
       <el-tab-pane label="日漫" name="manga" />
       <el-tab-pane label="韩漫" name="hanman" />
       <el-tab-pane label="其他" name="another" />
@@ -36,9 +36,11 @@ const selectedWeek = ref('')
 const activeType = ref('manga')
 const items = ref([])
 const loading = ref(false)
+const initLoading = ref(true)
+const ready = ref(false)
 
 const loadData = async () => {
-  if (!selectedWeek.value) return
+  if (!selectedWeek.value || !ready.value) return
   loading.value = true
   try {
     const data = await getWeekFilter(selectedWeek.value, activeType.value, 0)
@@ -47,15 +49,20 @@ const loadData = async () => {
   finally { loading.value = false }
 }
 
+const onWeekChange = () => loadData()
+const onTabChange = () => loadData()
+
 onMounted(async () => {
   try {
     const cats = await getWeekCategories()
     weekCategories.value = cats || []
     if (weekCategories.value.length > 0) {
       selectedWeek.value = weekCategories.value[0].id
-      loadData()
+      ready.value = true
+      await loadData()
     }
   } catch (e) { weekCategories.value = [] }
+  finally { initLoading.value = false }
 })
 </script>
 
